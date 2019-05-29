065cfbb4e179afc22fc302743256874e50f28987
Samuel Martin <Samuel Martin>
2019-05-29 11:09:05 +0100

    working perceptron class, which does no take df as argument.

--- perceptron_class.py
+++ perceptron_class.py
@@ -6,28 +6,27 @@ from random import random
 
 
 class Perceptron:
-    def __init__(self, training_dataframe):
+    def __init__(self):
         self.w1 = random()
         self.w2 = random()
         self.b = random()
         self.x1 = 0
         self.x2 = 0
         self.rate = 0.1
-        self.df = training_dataframe
+        self.df = pd.DataFrame(np.random.randint(0,1,
+                    size=(1, 3)), columns=['x1', 'x2', 'label'])
 
     def predict(self):
-        guess = self.x1 * self.w1 + self.x2 * self.w2 + self.b
-        return guess
-
-    def activation(self):
         """
         Taking imputs x1 w1 + x2 w2 + b, this returns either 1 or 0
         depending on the threshold: <= 0
         """
-        if self.predict() <= 0:
+        guess = self.x1 * self.w1 + self.x2 * self.w2 + self.b
+        if guess <= 0:
             return 0
         else:
             return 1
+
 
     def train(self):
         for x1, x2, label in self.df.itertuples(index=False):
@@ -45,6 +44,7 @@ class Perceptron:
 
 
 def test():
+    # make test data
     df = pd.DataFrame(np.random.randint(0,100,size=(10, 2))
                     , columns=['x1', 'x2'])
     labels = []
@@ -55,12 +55,13 @@ def test():
             labels.append(1)
     df['label'] = labels
 
-    p = Perceptron(df)
-    print(p.predict())
-    print(p.activation())
-    p.x1 = 0
-    p.x2 = 0
+    # make instantiate perceptron
+    p = Perceptron()
+    # feed it the test data
+    p.df = df
+    print(p.x1, p.w2, p.b)
     p.train()
+    print(p.x1, p.w2, p.b)
 
 
 if __name__ == '__main__':
